82133c992336dc029019f8d71dad4683b247d9f6
nzetzl <nzetzl@purdue.edu>
2016-02-20 12:18:55 -0600

    added twilio functionality and added code to send text message

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ import RPi.GPIO as GPIO
 
 import picamera
 
+from twilio.rest import TwilioRestClient
+
+account = "AC54f0af10d5a1f55ca9ce2fce1f8c06f0"
+token = "701c8da55ae6f912a5815a76d1fb87f6"
+client = TwilioRestClient(account, token)
+
 camera = picamera.PiCamera();
 
+camera.capture('img01.jpg')
 camera.start_recording('video.h264')
 sleep(5)
 camera.stop_recording()
+
+message = client.messages.create(to="+13176051723", from_="+13178303149" body="Hello!")
