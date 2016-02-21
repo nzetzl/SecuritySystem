42d8cb92f3b292b89b4f88b065c3899e12c855c8
nzetzl <nzetzl@purdue.edu>
2016-02-21 04:54:34 -0600

    added functionality to change .h264 videos (from the Pi) to .mp4

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import requests
+import subprocess
+
+subprocess.call(["MP4Box", "-add", "video.h264", "video.mp4"])
 
 r = requests.post('https://api.mogreet.com/cm/media.upload?client_id=7225&token=2449acf48398a0887e21d96de8369911&url=./video.mp4&type=video&name=securityVideo')
 
